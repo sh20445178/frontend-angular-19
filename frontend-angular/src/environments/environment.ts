60f4dfd8d0b72f9725351d0ab39721fb557c5583
ganatan <dannyganatan@gmail.com>
2025-05-29 14:51:13 +0200

    Start Angular 20 Update

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     application:
     {
       name: 'angular-bootstrap',
-      version: 'Angular 19.0.1',
-      bootstrap: 'Bootstrap 5.3.3',
-      fontawesome: 'Font Awesome 6.7.1',
+      version: 'Angular 20.0.0',
+      bootstrap: 'Bootstrap 5.3.6',
+      fontawesome: 'Font Awesome 6.7.2',
     }
   };
